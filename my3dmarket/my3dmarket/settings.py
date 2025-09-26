ea7f1a709057a36a7cdd52c0a9c409b9b1cf3d2c
nobphadonsr-tech <nobphadon.sr@gmail.com>
2025-09-26 13:55:07 +0700

    Upload My 3D Market v3

--- my3dmarket/my3dmarket/settings.py
+++ my3dmarket/my3dmarket/settings.py
@@ -12,29 +12,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-import environ
-
-env = environ.Env(
-    # Set cast types and default values
-    DJANGO_SECRET_KEY=str,
-    DEBUG=(bool, False)
-)
 
 # Take environment variables from .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='django-insecure-key')
+SECRET_KEY = 'jango-insecure-qg5qrzg5t+ddysg@6xfbzi3p%$9yf%vueu2a(s((d6xqm-2=$g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', default=True)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
